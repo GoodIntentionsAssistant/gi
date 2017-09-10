6aa6265ea9dffa802d95ea3266644b9c58f88cb2
Darren Moore <darren.m@firecreekweb.com>
2017-09-10 20:36:30 +0700

    Resolves #90 - Log to file option in config

--- src/Core/log.js
+++ src/Core/log.js
@@ -61,6 +61,11 @@ module.exports = class Log {
 * @return void
 */
   write_log(type, text) {
+    //Disabled logging
+    if(!this.app.Config.read('logging.enabled')) {
+      return false;
+    }
+
     var filename = this.app.Path.get('logs')+'/'+type+'/'+moment().format('MM-DD-YYYY')+'.txt'
     var line = moment().format('MM-DD-YYYY HH:mm:ss')+': '+text+"\n";
 
